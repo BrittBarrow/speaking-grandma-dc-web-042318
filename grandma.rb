ffaef0cc5afb2e7b914853f2ca6cb45a3d0806ba
Brittany Barrow <bangelabarrow@gmail.com>
2018-04-14 02:24:29 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma (string)
   string = "I LOVE YOU GRANDMA!"
- if string.upcase() == string
+ if string.upcase == string
     puts "I LOVE YOU TOO PUMPKIN!"
 end
 
